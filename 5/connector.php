a83eb996a16bad97eb697c87747c0728618cf369
Дмитрий Курляк <cat1020486@gmail.com>
2022-03-24 00:03:32 +0300

    @fix connector.php changed to modern connection

--- 5/connector.php
+++ 5/connector.php
@@ -1,10 +1,15 @@
 <?php
-global $dbh;
-try {
-    $user = "root";
-    $pass = "admin";
-    $dbh = new PDO('mysql:host=localhost;dbname=intervolga_country', $user, $pass);
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+
+$host = 'localhost';
+$db = 'intervolga_country';
+$user = 'root';
+$pass = 'admin';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$dbh = new PDO($dsn, $user, $pass, $opt);
